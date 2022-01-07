2d6db72522159e3dafff8c787a012dbf1aca8d14
philippvlasov <pylyp.vlasov@valtech.com>
2022-01-07 10:29:54 +0200

    Change logic for applying active news card styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,11 @@
-const newsBlock = document.getElementById('news');
-// const newsCard = newsBlock.querySelector('.news__card');
-const cardsArray = Array.from(document.querySelectorAll('.news__card'))
+const newsBlock = document.querySelector('.news__cards')
+const cardsArray = Array.from(newsBlock.querySelectorAll('.news__card'))
 
 cardsArray.map(item => {
-    console.log(item)
     item.addEventListener("click", (e) => {
         //Отменить дефолтное действие (переход по ссылке)
-        e.preventDefault();
-        //Проходит по картам массива и убирает класс _big если он есть 
-        deleteClass(cardsArray);
-        //Добавляет класс кликнутому элементу
-        item.classList.add('_big')
+        e.preventDefault()
+
+        newsBlock.prepend(item)
     })
 })
-
-const deleteClass = (array) => {
-    array.map((item) => {
-        item.classList.remove('_big')
-    })
-}
